87297bfa0bd61fabb2e03a8dec4f38dc0e41597c
Priptide <priptidehelp@gmail.com>
2021-12-26 15:17:01 +0000

    Styling and adding of biography page

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,35 @@ function App() {
         {
           snapDestinationY: "90%",
         },
-        () => console.log("snapped")
+        () => console.log(container)
       );
 
     return;
   }, []);
 
   return (
+    // TODO: Remove the scroll bar from showing.
+    // TODO: Make better scroll between screens.
+    // TODO: Components bio page.
+    // TODO: Add links too the bio page.
+
     <div id="container" ref={container}>
       <div className="page first-page">
-        <div>I</div>
-        <div className="hint">scroll down</div>
+        <div>hi</div>
+        <div className="hint">
+          <div >scroll down</div>
+        </div>
       </div>
       <div className="page second-page">
-        <div>II</div>
+        <div>i am Charlie</div>
       </div>
       <div className="page third-page">
-        <div>III</div>
+        <div className="bio">{'I am a computer science student in the united kingdom'}<br/>{'I love to write code, working recently with AI and ML as well as multi-platform app development.'}<br/>{'Having been programming for over 10 years, starting with game development in pygame, I am always trying to improve although I will be the first to admit there are many people much better than me and there is always more too learn.'}<br/>{'Below are some of my favorite sources of development knowledge and I would highly recommend checking them out.'}</div>
+        <div className="links">
+          <button>CSS Videos</button>
+          <button>Leet Code</button>
+          <button>CSDojo</button>
+        </div>
       </div>
       <div className="page fourth-page">
         <div>IV</div>
